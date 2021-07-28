29594bf4021bac5695d0804d177be65a83fe71f6
Robert <nz.r.hughes@gmail.com>
2021-07-29 08:44:08 +1200

    first commit

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <gint/display.h>
+#include <gint/keyboard.h>
+#include <stdio.h>
+
+int main(void)
+{
+	dclear(C_WHITE);
+	int mat[4][1] = {
+		{1},
+		{2},
+		{3},
+		{4}
+	};
+	int num_rows = sizeof(mat) / sizeof(mat[0]);
+	dprint(1,1,C_BLACK,"%s");
+	dupdate();
+
+	getkey();
+	return 1;
+}
