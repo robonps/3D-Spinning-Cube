2d54aeae7fe1a7a206e1a8e00ca3810fffb0521f
Robert <nz.r.hughes@gmail.com>
2021-07-30 07:35:32 +1200

    Fixed matrix multiplier function

--- src/main.c
+++ src/main.c
@@ -2,21 +2,21 @@
 #include <gint/keyboard.h>
 #include <stdio.h>
 
-void matmul(int mat1[][10], int mat2[][10]);
+void matmul(int mat1[10][10], int mat2[10][10], int row1, int row2, int col1, int col2);
 int mult[10][10];
 
 int main(void){
 	dclear(C_WHITE);
-	int points[][10] = {
+	int points[10][10] = {
 		{1,2},
 		{2,3},
 	};
-	int matrixthing[][10] = {
+	int matrixthing[10][10] = {
 		{6, 5},
 		{5, 6},
 	};
 	char answer[10];
-	matmul(matrixthing, points);
+	matmul(matrixthing, points,2,2,2,2);
 	sprintf(answer, "%d", mult[0][0]);
 	dprint(1,1,C_BLACK,answer);
 	dupdate();
@@ -26,31 +26,25 @@ int main(void){
 }
 
 // Function to multiply both matrices
-void matmul(int mat1[][10], int mat2[][10]){
-
-	// Finding row size and column size for both mat1 and mat2
-	const int row1 = sizeof(mat1) / sizeof(mat1[0]);
-	const int col1 = sizeof(mat1[0]) / sizeof(mat1[0][0]);
-
-	const int row2 = sizeof(mat2) / sizeof(mat2[0]);
-	const int col2 = sizeof(mat2[0]) / sizeof(mat2[0][0]);
+void matmul(int mat1[10][10], int mat2[10][10], int row1, int row2, int col1, int col2){
 
 	if(col1 != row2){
 		return;
 	}
 
-	int i,j,k;
+	int i,j,k,sum;
 
 	// Multiplying matrix firstMatrix and secondMatrix and storing in array mult.
 	for(i = 0; i < row1; ++i)
 	{
 		for(j = 0; j < col2; ++j)
 		{
-			mult[i][j] = 0;
+			sum = 0;
 			for(k=0; k<col1; ++k)
 			{
-				mult[i][j] += mat1[i][k] * mat2[k][j];
+				sum += mat1[i][k] * mat2[k][j];
 			}
+			mult[i][j] = sum;
 		}
 	}
 }
